e7c58461f8109fa20f7ca122e0cbaf9843d61f2e
Kim Jae Jun <rlawowns97@naver.com>
2024-08-17 01:29:22 +0900

    RH-2 : 메인페이지 및 그 서브페이지들에 대한 route 정의 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/page';
+import CustomerSupportPage from './pages/customer_support/page';
+import OurStoryPage from './pages/our_story/page';
+import MainServicePage from './pages/main_service/page';
+import ReHubTemplatesPage from './pages/re_hub_templates/page';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <div>회고서비스 시작</div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/main-service" element={<MainServicePage />} />
+        <Route path="/rehub-templates" element={<ReHubTemplatesPage />} />
+        <Route path="/our-story" element={<OurStoryPage />} />
+        <Route path="/customer-support" element={<CustomerSupportPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
