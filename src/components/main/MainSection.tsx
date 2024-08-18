1efc70e0dc2ecfad1d79e3f483c1e9efb37bd665
Kim Jae Jun <rlawowns97@naver.com>
2024-08-18 21:28:33 +0900

    RH-2 : MainSection markup 작업

---
+++ src/components/main/MainSection.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './MainSection.module.css';
+
+const MainSection: React.FC = () => {
+  return (
+    <section className={styles.mainSection}>
+      <div className={styles.leftMainSection}>
+        <div>
+          <h1 className={styles.title} lang={lang.en}>ReHub(Retrospect Hub)</h1>
+          <h2 className={styles.subtitle}>당신이 원하는 회고 서비스</h2>
+        </div>
+        <div className={styles.startBtnWrapper}>
+          <button className={styles.startBtn}>시작하기</button>
+        </div>
+      </div>
+
+      <div>
+        {/*<img*/}
+        {/*  className={styles.mainImage}*/}
+        {/*  src={`${process.env.PUBLIC_URL}/images/meeting_img.jpg`}*/}
+        {/*  alt="Illustration of people discussing"*/}
+        {/*/>*/}
+      </div>
+    </section>
+  );
+};
+
+const lang = {
+  ko: 'ko',
+  en: 'en',
+};
+
+export default MainSection;
