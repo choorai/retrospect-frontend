091bf6c07ea131422df7f94f95dfaf586f682c2e
Kim Jae Jun <rlawowns97@naver.com>
2024-08-21 02:21:47 +0900

    RH-6 : 회고 목표 설정 페이지 개발 (슬라이드 첫번째 요소)

--- src/pages/retrospect-setting/page.tsx
+++ src/pages/retrospect-setting/page.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+import FirstSetting from '../../components/retrospect-settings/FirstSetting';
+// import { baseUrl } from "./config";
 
 const RetrospectSettingsPage = () => {
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  // };
   return (
-    <h1>Here is RetrospectSettingsPage</h1>
+    <section>
+      {/* <StatusBar /> */}
+      <FirstSetting />
+      {/* <progressBtnSection /> */}
+
+
+      {/*<Slider {...settings}>*/}
+      {/*  <div>*/}
+      {/*    <h3 style={{ color: '#FFFAFF', backgroundColor: '#ABD1A9' }}>1</h3>*/}
+      {/*  </div>*/}
+      {/*  <div>*/}
+      {/*    <h3 style={{ color: '#FFFAFF', backgroundColor: '#ffba13' }}>2</h3>*/}
+      {/*  </div>*/}
+      {/*  <div>*/}
+      {/*    <h3 style={{ color: '#FFFAFF', backgroundColor: '#00fff7' }}>3</h3>*/}
+      {/*  </div>*/}
+      {/*  <div>*/}
+      {/*    <h3 style={{ color: '#FFFAFF', backgroundColor: '#ff2141' }}>4</h3>*/}
+      {/*  </div>*/}
+      {/*  <div>*/}
+      {/*    <h3 style={{ color: '#FFFAFF', backgroundColor: '#4a3eff' }}>5</h3>*/}
+      {/*  </div>*/}
+      {/*</Slider>*/}
+    </section>
   );
 };
 
