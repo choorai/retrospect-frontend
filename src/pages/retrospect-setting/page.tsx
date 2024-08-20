2d432dbb0807be26aafbda5815a3c5c1db02710e
Kim Jae Jun <rlawowns97@naver.com>
2024-08-21 01:26:35 +0900

    RH-6 : 회고 시작 전 셋팅 화면 추가 및 routing

---
+++ src/pages/retrospect-setting/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RetrospectSettingsPage = () => {
+  return (
+    <h1>Here is RetrospectSettingsPage</h1>
+  );
+};
+
+export default RetrospectSettingsPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import mainServiceRoutes from './MainServiceRoutes';
 import reHubTemplateRoutes from './ReHubTemplatesRoutes';
 import ourStoryRoutes from './OurStoryRoutes';
 import customerSupportRoutes from './CustomerSupportRoutes';
+import retrospectSettingsRoutes from './RetrospectSettingsRoutes';
 
 const routes: RouteObject[] = [
   ...homeRoutes,
@@ -11,6 +12,7 @@ const routes: RouteObject[] = [
   ...reHubTemplateRoutes,
   ...ourStoryRoutes,
   ...customerSupportRoutes,
+  ...retrospectSettingsRoutes
 ];
 
 export default routes;
