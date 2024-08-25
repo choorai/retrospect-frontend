35e8eea85c77b86e8bf4f40291c69bc366f05957
Seokju Hong <iamseokjuhong@gmail.com>
2024-08-25 19:42:53 +0900

    RH-2: 로그인 컴포넌트+css 작업

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,21 +1,12 @@
 import MainNavBar from '../../components/main/MainNavBar';
 import React from 'react';
+import Login from '../../components/login/Login';
 
 const LoginPage = () => {
   return (
     <>
       <MainNavBar />
-      <div>로그인을 하면 회고를 저장할 수 있어요.</div>
-      <div>
-        <form action="" method="post">
-          <label htmlFor="id">아이디</label>
-          <input id="id" type="text" />
-          <label htmlFor="password">비밀번호</label>
-          <input id="password" type="password" />
-        </form>
-        <div><a href="">회원가입</a></div>
-        <div><a href="">아이디/비밀번호 찾기</a></div>
-      </div>
+      <Login />
     </>
   );
 };
