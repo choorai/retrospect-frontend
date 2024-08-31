f3ecb0e61d55ea42669c1777557450927531bfad
Seokju Hong <iamseokjuhong@gmail.com>
2024-08-31 12:22:55 +0900

    RH-4: 불필요한 태그 제거

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,44 +3,42 @@ import styles from '../../styles/login/Login.module.css';
 
 const Login: React.FC = () => {
   return (
-    <>
-      <section className={styles.rootContainer}>
-        <div className={styles.content}>
-          <div className={styles.title}>로그인을 하면 회고를 저장할 수 있어요.</div>
-          <div className={styles.inputContainer}>
-            <form action="" method="post">
-              <label htmlFor="id">아이디</label>
-              <input
-                className={`${styles.inputComponent} ${styles.inputMargin}`}
-                id="id"
-                type="text"
-                placeholder={'아이디'}
-              />
-              <label htmlFor="password">비밀번호</label>
-              <input
-                className={`${styles.inputComponent} ${styles.inputMargin}`}
-                id="password"
-                type="password"
-                placeholder={'비밀번호'}
-              />
-              <input
-                className={`${styles.inputComponent} ${styles.inputMargin} ${styles.loginButton}`}
-                type="button"
-                value={'로그인'}
-              />
-            </form>
-          </div>
-          <ul className={`${styles.sub}`}>
-            <li>
-              <a href="">회원가입</a>
-            </li>
-            <li>
-              <a href="">아이디/비밀번호 찾기</a>
-            </li>
-          </ul>
+    <section className={styles.rootContainer}>
+      <div className={styles.content}>
+        <div className={styles.title}>로그인을 하면 회고를 저장할 수 있어요.</div>
+        <div className={styles.inputContainer}>
+          <form action="" method="post">
+            <label htmlFor="id">아이디</label>
+            <input
+              className={`${styles.inputComponent} ${styles.inputMargin}`}
+              id="id"
+              type="text"
+              placeholder={'아이디'}
+            />
+            <label htmlFor="password">비밀번호</label>
+            <input
+              className={`${styles.inputComponent} ${styles.inputMargin}`}
+              id="password"
+              type="password"
+              placeholder={'비밀번호'}
+            />
+            <input
+              className={`${styles.inputComponent} ${styles.inputMargin} ${styles.loginButton}`}
+              type="button"
+              value={'로그인'}
+            />
+          </form>
         </div>
-      </section>
-    </>
+        <ul className={`${styles.sub}`}>
+          <li>
+            <a href="">회원가입</a>
+          </li>
+          <li>
+            <a href="">아이디/비밀번호 찾기</a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 };
 
